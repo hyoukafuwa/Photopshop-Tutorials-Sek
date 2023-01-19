2c80f199cac14e14c33398ab3d7d106a1fea62a6
ozu <sam1sepiol@proton.me>
2023-01-19 12:15:40 +0100

    fixed dotenv issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+// import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  process.env.VUE_APP_API_KEY,
-  process.env.VUE_APP_AUTH_DOMAIN,
-  process.env.VUE_APP_PROJECT_ID,
-  process.env.VUE_APP_STORAGE_BUCKET,
-  process.env.VUE_APP_MESSGING_SENDER_ID,
-  process.env.VUE_APP_APP_ID_1,
-  process.env.VUE_APP_MEASUREMENT_ID
-};
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messageSenderId: import.meta.env.VITE_MESSGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+// const analytics = getAnalytics(app)
+
+export { auth }
