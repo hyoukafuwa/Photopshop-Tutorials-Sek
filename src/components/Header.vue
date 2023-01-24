47bb707b5ead97058a537f1e56029a0595fa5410
ozu <sam1sepiol@proton.me>
2023-01-24 08:52:19 +0100

    added add post button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { auth } from '../firebase'
+import { signOut } from '@firebase/auth'
+
+const props = defineProps({
+  isLoggedIn: Boolean
+})
+
+const emit = defineEmits(['openAuthModal', 'showOrHideSearchBar', 'signedOut'])
+
+// sign out when confirm prompt returns a positive value
+function handleSignOut() {
+  if (confirm('Möchtest du dich wirklich abmelden?')) {
+    signOut(auth).then(() => {
+      emit('signedOut')
+    })
+  }
+}
 </script>
 
 <template>
@@ -10,12 +27,19 @@ import { RouterLink } from 'vue-router';
       </RouterLink>
     </h3>
     <nav class="header__nav">
-      <button @click="$emit('showOrHideSearchBar')" class="header__nav-search">
+      <button @click="emit('showOrHideSearchBar')" class="header__nav__search">
         <i class="fa-solid fa-magnifying-glass"></i>
       </button>
-      <button @click="$emit('openAuthModal')" class="header__nav-login">
+      <button v-if="!isLoggedIn" @click="emit('openAuthModal')" class="header__nav__login">
         <i class="fa-solid fa-user"></i>
       </button>
+      <button v-if="isLoggedIn" @click="handleSignOut" class="header__nav__log-out">
+        <i class="fa-solid fa-right-from-bracket"></i>
+      </button>
+      <RouterLink v-if="isLoggedIn" to="/create-post" class="header__nav__create-post">
+        <i class="fa-solid fa-plus"></i>
+        <span>Add Post</span>
+      </RouterLink>
     </nav>
   </header>
 </template>
@@ -41,12 +65,13 @@ import { RouterLink } from 'vue-router';
   }
 
   &__nav {
-    width: 50px;
+    // width: 50px;
     display: flex;
+    gap: 10px;
     @include base.justify-align(space-between, center);
 
-    // target all elements in nav
-    & > * {
+    // target button elements in nav
+    &__search, &__login, &__log-out {
       @include base.transition(color);
       // remove all the standard button css styling
       @include base.remove-button-styling;
@@ -57,6 +82,17 @@ import { RouterLink } from 'vue-router';
         @include base.transition(color);
         color: base.$hover-blue;
       }
+    }
+
+    &__create-post {
+      display: flex;
+      font-size: 0.9rem;
+      gap: 5px;
+      @include base.remove-anchor-styling;
+      @include base.button-styling;
+      @include base.justify-align();
+      height: 35px;
+      width: 90px;
     }
   }
 }
