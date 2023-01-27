e745471adac82c1f888541f0a6b498a89a0a0e0f
ozu <sam1sepiol@proton.me>
2023-01-27 08:53:29 +0100

    added new page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
+import AddPostView from '../views/AddPostView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,11 @@ const router = createRouter({
       name: 'main',
       component: MainView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/add-post',
+      name: 'add-post',
+      component: AddPostView
+    }
   ]
 })
 
