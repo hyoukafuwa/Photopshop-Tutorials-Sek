cd45a3f3d004d8977adc77d3726cc61172c189ff
ozu <sam1sepiol@proton.me>
2023-01-27 10:58:10 +0100

    added new route options

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import MainView from '../views/MainView.vue'
 import AddPostView from '../views/AddPostView.vue'
+import ArticleView from '../views/ArticleView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: MainView
+      name: 'Main',
+      component: MainView,
     },
     {
       path: '/add-post',
-      name: 'add-post',
+      name: 'AddPost',
       component: AddPostView
-    }
+    },
+    {
+      path: '/article/:id',
+      name: 'Article',
+      component: ArticleView,
+      // beforeEnter: async (to, from, next) => {
+      //   const articleId = to.params.id
+      //   console.log(articleId)
+      //   const docSnap = await getDoc(doc(db, 'tutorials', articleId))
+      //   if (docSnap) return next()
+      //   next({ name: 'NotFound' })
+      // }
+    },
+    { 
+      path: '/:pathMatch(.*)*', 
+      name: 'NotFound', 
+      component: NotFoundView 
+    },
   ]
 })
 
