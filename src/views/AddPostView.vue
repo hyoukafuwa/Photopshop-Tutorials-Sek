2e120b27b624bd9d3d523f04b4a981e54fc66315
ozu <sam1sepiol@proton.me>
2023-01-27 08:54:34 +0100

    added new view for adding articles

---
+++ src/views/AddPostView.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { reactive, ref, onMounted } from 'vue'
+import { onAuthStateChanged } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../firebase'
+import { auth } from '../firebase'
+import { useRouter } from 'vue-router'
+
+import createDateString from '../utils/createDateString'
+import Article from '../components/Article.vue'
+
+import getYouTubeID from 'get-youtube-id'
+
+const router = useRouter()
+const userLoggedIn = ref()
+
+onMounted(() => {
+  // if the user is not logged in, he shall be sent back to the front page
+  onAuthStateChanged(auth, user => {
+    if (user) userLoggedIn.value = true
+    else router.push('/')
+  })
+})
+
+const articleData = reactive({
+  title: '',
+  youTubeUrl: '',
+  description: ''
+})
+
+// whenever the input is clear it shall use 'Beispieltext' as a placeholder
+function asignTitle() {
+  if (articleData.title) return articleData.title
+  return 'Beispieltitel'
+}
+
+async function addPostToDB() {
+  // return an alert if one of the fields is without any value
+  for (const val of Object.values(articleData)) {
+    if (!val) return alert('Eines der Felder ist leer')
+  }
+
+  // check if provided youtube url exists and get its id
+  const youTubeId = getYouTubeID(articleData.youTubeUrl)
+  if (youTubeId == null) alert('die angegebene YouTube-URL ist falsch')
+
+  const creationDate = new Date()
+
+  if (!confirm('Hiermit bestätigen Sie, dass Sie diesen Artikel in dieser Form abschicken')) return
+
+  await addDoc(collection(db, 'tutorials'), {
+    title: articleData.title,
+    date: {
+      day: creationDate.getDate(),
+      month: creationDate.getMonth() + 1,
+      year: creationDate.getFullYear()
+    },
+    youTubeUrl: youTubeId
+  })
+
+  // reset all fields
+  articleData.title = ''
+  articleData.youTubeUrl = ''
+  articleData.description = ''
+}
+</script>
+
+<template>
+  <main class="add-post">
+    <form @submit.prevent="addPostToDB()" class="add-post__form">
+      <span>
+        <label for="add-post__form__title-input">Titel</label>
+        <input v-model="articleData.title" type="text" placeholder="Tutorial..." class="add-post__form__title-input">
+      </span>
+      <span>
+        <label for="add-post__form__yt-url-input">YouTube-URL</label>
+        <input v-model="articleData.youTubeUrl" type="text" placeholder="https://youtu.be/dQw4w9WgXcQ" class="add-post__form__yt-url-input">
+      </span>
+      <span>
+        <label for="add-post__form__description-input">Beschreibung</label>
+        <textarea v-model="articleData.description" type="text" maxlength="200" placeholder="Es geht um..." class="add-post__form__description-input"></textarea>
+        <i class="add-post__form__description-char-count">{{ articleData.description.length }}/200</i>
+      </span>
+      <button class="add-post__form__submit-article">Absenden</button>
+    </form>
+    <section class="add-post__preview">
+      <Article :title="asignTitle()" :date="createDateString(new Date())"/>
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+@use '../assets/base';
+
+.add-post {
+  min-height: 100vh;
+  gap: 100px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: row;
+  @include base.justify-align(center, flex-start);
+
+  &__form {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+      margin-bottom: 20px;
+
+      input {
+        width: 400px;
+        height: 35px;
+        @include base.input-styling;
+      }
+
+      textarea {
+        font-family: base.$font;
+        padding: 5px;
+        min-width: 400px;
+        max-width: 400px;
+        min-height: 140px;
+        @include base.input-styling;
+      }
+
+      // move char count to end
+      i {
+        align-self: flex-end
+      }
+    }
+
+    &__submit-article {
+      @include base.button-styling;
+      height: 40px;
+    }
+  }
+}
+
+@media(max-width: 1024px) {
+  .add-post {
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 100px;
+  }
+}
+
+</style>
