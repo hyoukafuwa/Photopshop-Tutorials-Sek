7afcd8785746eba5b3b62ed553a313629f3cd2ee
hyoukafuwa <sam1sepiol@proton.me>
2023-02-05 19:33:34 +0100

    finished article view

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,36 +1,101 @@
-<!-- <script setup>
-import { doc, getDoc } from 'firebase/firestore'
-import { db } from '../firebase'
-import { ref, onMounted, watch } from 'vue'
-import { useRoute } from 'vue-router'
+<script setup>
+import { ref, watch } from 'vue'
+import { state } from '../state/state'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 
-const articleId = ref(route.params.articleId)
-const article = ref()
-const articleNotExisting = ref(false)
+import ArticleCard2 from '../components/ArticleCard2.vue'
+import ArticleSection from '../components/ArticleSection.vue'
+
 const route = useRoute()
+const articleId = route.params.articleId
 
-// onMounted(async () => {
-//   const docRef = doc(db('tutorials', articleId))
-//   const docSnap = await getDoc(docRef)
+let otherArticles = ref([])
 
-//   if (!docSnap.exists()) return articleNotExisting.value = true
-//   article.value = docSnap.data()
-// })
+function getFittingArticles(id) {
+  otherArticles.value = []
+  for (let i = 0; i < 3; i++) {
+    const stateArticles = state.articles.filter(article => article.id !== id)
+    otherArticles.value.push(stateArticles[i])
+  }
+} 
 
-// watch(articleId, async (toParams, previousParams) => {
-//   const docRef = doc(db('tutorials', toParams))
-//   const docSnap = await getDoc(docRef)
+getFittingArticles(articleId)
 
-//   if (!docSnap.exists()) return articleNotExisting.value = true
-//   article.value = docSnap.data()
-// })
+let mainArticle = ref(state.articles.filter(article => article.id === articleId)[0])
 
-console.log(articleNotExisting)
+onBeforeRouteUpdate((to, from) => {
+  mainArticle.value = state.articles.filter(article => article.id === to.params.articleId)[0]
+  getFittingArticles(to.params.articleId)
+})
 
-</script> -->
+</script>
 
 <template>
-
-<h1>test</h1>
+  <div v-if="otherArticles !== []" class="article-view">
+    <ArticleSection :article="mainArticle"/>
+    <aside class="article-view__aside">
+      <span class="article-view__aside__title">
+        <h2>Andere Tutorials</h2>
+      </span>
+      <section class="article-view__aside__article-container">
+        <ArticleCard2 v-for="article in otherArticles" :title="article.title" :date="article.date" :id="article.id"/>
+      </section>
+    </aside>
+  </div>
+  <span v-else>Loading...</span>
 
 </template>
+
+<style lang="scss" scoped>
+@use '@/assets/base';
+
+.article-view {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 60px;
+  min-height: 100vh;
+  width: 100%;
+
+  &__aside {
+    margin: 0 10px;
+
+    &__title {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    &__article-container {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    }
+  }
+}
+
+@media (max-width: 1400px) {
+  .article-view {
+    flex-direction: column;
+    align-items: center;
+
+    &__aside {
+      width: 100%;
+      min-width: 450px;
+      display: flex;
+      flex-direction: column;
+      margin: 40px 0;
+
+      &__title {
+        align-self: center;
+        margin-bottom: 20px;
+      }
+
+      &__article-container {
+        display: grid;
+        justify-content: center;
+        grid-template-columns: repeat(auto-fit, 450px);
+      }
+    }
+  }
+}
+
+</style>
