890650380a946c22f739b5f96c412bf44f747a01
hyoukafuwa <sam1sepiol@proton.me>
2023-02-05 23:54:02 +0100

    adjusted file for docker

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
   }
 })
