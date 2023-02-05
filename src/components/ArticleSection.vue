eb56b31df86c59eafa1d64eec5a2af3ad4010eb1
hyoukafuwa <sam1sepiol@proton.me>
2023-02-05 04:26:10 +0100

    added grid css

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -1,54 +1,36 @@
 <script setup>
 import { useRoute } from 'vue-router'
-import { doc, getDoc } from 'firebase/firestore'
-import { db } from '../firebase'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
+import { state } from '../state/state'
 
 const route = useRoute()
 const articleId = route.params.articleId
-const article = ref({})
 
-const props = defineProps({
-  isLoggedIn: {
-    type: Boolean,
-    required: true
-  }
-})
-
-onMounted(async () => {
-  const docRef = doc(db, 'tutorials', articleId)
-  const docSnap = await getDoc(docRef)
-
-  const articleData = docSnap.data()
-
-  article.value = {
-    title: articleData.title,
-    description: articleData.description,
-    date: articleData.date,
-  }
-})
+// get the one article form the array that has the site params's id
+const article = ref(state.articles.filter(article => article.id === articleId)[0])
+console.log(article.value)
 
 </script>
 
 <template>
   <main v-if="article" class="article-section">
     <h1 class="article-section__title">
-      {{ article.itle }}
+      {{ article.title }}
     </h1>
     <span class="article-section__date">
-      {{ article.dates }}
+      {{ article.date }}
     </span>
     <section class="article-section__video">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/5N_fTRZuTkc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/5N_fTRZuTkc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </section>
+    <button class="article-section__button" v-if="state.isLoggedIn">
+      Delete
+    </button>
     <section class="article-section__description">
       <p class="article-section__article__description__paragraph">
         {{ article.description }}
       </p>
     </section>
-    <button class="article-section__button" v-if="isLoggedIn">
-      Delete
-    </button>
   </main>
 </template>
 
@@ -56,29 +38,59 @@ onMounted(async () => {
 @use '@/assets/base';
 
 .article-section {
-    width: 600px;
-    // min-width: 600px;
-    @include base.box-shadow;
-    // display: flex;
-    // flex-direction: column;
-    display: grid;
+  max-width: 900px;
+  height: 800px;
+  // min-width: 600px;
+  @include base.box-shadow;
+  // display: flex;
+  // flex-direction: column;
+  display: grid;
+  grid-template-columns: 50px repeat(2, 4fr) 50px;
+  grid-template-rows: 80px minmax(300px, 500px) 40px minmax(60px, 200px);
 
-    &__video {
+  &__video {
+    grid-column: 2 / 4;
+    // grid-row-end: 1;
+    // justify-self: center;
+    display: flex;
+    align-items: center;
 
-    }
-
-    &__title {
-      // align-self: center;
-      font-size: 2.2rem;
-      margin-bottom: 10px;
-    }
-
-    &__description {
-      // justify-self: flex-start;
-    }
-
-    &__button {
+    iframe {
+      // width: 500px;
+      // height: 300px;
+      width: 100%;
+      height: 90%;
     }
   }
+
+  &__title {
+    // align-self: center;
+    font-size: 2.2rem;
+    margin-bottom: 10px;
+    grid-column-start: 2;
+  }
+
+  &__date {
+    justify-self: end;
+    grid-column-start: 3
+  }
+
+  &__description {
+    grid-row-start: 4;
+    grid-column: 2 / 4;
+    align-self: center;
+    padding-right: 20px;
+  }
+
+  &__button {
+    @include base.button-styling(base.$red, base.$hover-red);
+    width: 100px;
+    height: 30px;
+    justify-self: end;
+    grid-row-start: 3;
+    grid-column-end: 4;
+    align-self: center;
+  }
+}
 
 </style>
