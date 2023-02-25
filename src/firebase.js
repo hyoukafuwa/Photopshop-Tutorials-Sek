cfdcb3b6a9298fc7cccdda90dc2d018189346140
Ozu <sam1sepiol@proton.me>
2023-02-25 16:18:42 +0100

    missspellings and missing parameter

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,10 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messageSenderId: import.meta.env.VITE_MESSGING_SENDER_ID,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 }
